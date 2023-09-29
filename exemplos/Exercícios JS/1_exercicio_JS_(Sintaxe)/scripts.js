79b46b77d387b776bfd46ab84a28277a5c7e9035
estherramoss <estherramosbocchini@gmail.com>
2023-09-29 08:30:10 -0300

    .

--- exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
+++ exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
@@ -1,4 +1,11 @@
 document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
-// Digite a resolução do exercício aqui e visualize o resultado no navegador
+const nome = "Esther Ramos";
+let idade = "17 anos";
+var curso = "Técnico em Desenvolvimento de Sistemas";
+//Digite a resolução do exercício aqui e visualize o resultado no navegador
+
+document.write("<strong>Meu nome é: </strong>" +nome+ "<br>")
+document.write(`<strong>Idade: </strong>"${idade} <br>`)
+document.write(`<strong>Eu sou a: </strong> ${nome}; <strong> Tenho: </strong> ${idade}; <strong>Estou estudando no curso: </strong>${curso}<br>`)
--- exemplos/Exercícios JS/2_exercicio_JS_(Comparação)/scripts.js
+++ exemplos/Exercícios JS/2_exercicio_JS_(Comparação)/scripts.js
@@ -2,6 +2,9 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 document.write('1º Exemplo: <br><br>');
+document.write('5 ==5 && 8> 3');
+document.write('<br>Resultado:');
+document.write('5 ==5 && 8> 3');
 
 // Digite a resolução do 1º exemplo aqui e visualize o resultado no navegador
 
@@ -11,3 +14,4 @@ document.write('__________________________________________________________<br><b
 document.write('2º Exemplo: <br><br>');
 
 // Digite a resolução do 2º exemplo aqui e visualize o resultado no navegador
+document.write('1º Exemplo: <br><br>');
